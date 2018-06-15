88c8f4d92ebca2a9eb893897caad0e6850e45a9a
webinaneff <40257715+webinaneff@users.noreply.github.com>
2018-06-15 13:02:22 +0500

    new change

--- welcome.php
+++ welcome.php
@@ -30,7 +30,7 @@
 	            $results = $wpdb->get_results($sql);
 	            ?>
 
-<?php print_r($index); ?>
+<?php print_r($index1); ?>
 	            <?php print_r($index2); ?>
 	            <?php print_r($index3); ?>
 
